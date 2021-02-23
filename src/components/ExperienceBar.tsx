921d4f0081a6bb7cac975bb5f88c5985577e3847
vinicius <vinicius.faleiros@hotmail.com>
2021-02-23 16:32:41 -0300

    first commit

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,19 @@
+import '../styles/global.css'
+
+export const ExperienceBar: React.FC = () => {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: "50%" }} />
+
+        <span className="current-experience" style={{ left: "50%" }}>
+          300 xp
+          </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+};
+
+
