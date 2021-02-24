19dcc50d19355f04e2a3c812861033c498b7e1b5
Vinicius Faleiros <42284565+vinicius2616@users.noreply.github.com>
2021-02-24 00:39:02 -0300

    commit

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,19 +1,17 @@
-import '../styles/global.css'
+import styles from "../styles/components/ExperienceBar.module.css";
 
 export const ExperienceBar: React.FC = () => {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{ width: "50%" }} />
 
-        <span className="current-experience" style={{ left: "50%" }}>
+        <span className={styles.currentExperience} style={{ left: "50%" }}>
           300 xp
-          </span>
+        </span>
       </div>
       <span>600 xp</span>
     </header>
   );
 };
-
-
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
-import '../styles/global.css'
+import "../styles/global.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
